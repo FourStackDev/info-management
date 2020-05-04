8125c95415c1c8b22079668646083943dfcafed4
Hm <Manjunath_Hm@Dellteam.com>
2020-05-04 10:30:11 +0530

    Initial Commit

---
+++ src/test/java/org/fourstack/infomanagement/InfoManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fourstack.infomanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfoManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
