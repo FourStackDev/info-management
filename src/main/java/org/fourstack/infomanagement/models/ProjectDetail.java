5453bccf6d293699b30806ccc376a7e193b371ff
Hm <Manjunath_Hm@Dellteam.com>
2020-05-12 11:17:42 +0530

    updated @ManyToMany mappings for ProjectDetail and Technology Entites

--- src/main/java/org/fourstack/infomanagement/models/ProjectDetail.java
+++ src/main/java/org/fourstack/infomanagement/models/ProjectDetail.java
@@ -2,10 +2,28 @@ package org.fourstack.infomanagement.models;
 
 import java.io.Serializable;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+@Entity
+@Table(name = "project_details")
 public class ProjectDetail implements Serializable{
 
 	/**
@@ -13,19 +31,123 @@ public class ProjectDetail implements Serializable{
 	 */
 	private static final long serialVersionUID = -2434236462836489298L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@Column(name = "project_name")
+	@JsonProperty(value = "project_name")
 	private String projectName;
+	
+	@Column(name = "team_size")
+	@JsonProperty(value = "team_size")
 	private int teamSize;
+	
+	@DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+	@JsonFormat(pattern = "yyyy/MM/dd")
+	@Column(name = "start_date")
+	@JsonProperty(value = "start_date")
 	private LocalDate startDate;
+	
+	@DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+	@JsonFormat(pattern = "yyyy/MM/dd")
+	@Column(name = "end_date")
+	@JsonProperty(value = "end_date")
 	private LocalDate endDate;
 	
+	@Column(name = "project_description", columnDefinition = "TEXT")
 	@JsonProperty(value = "project_description")
 	private String description;
-	
-	@JsonProperty(value = "technologies_used")
-	private List<Technology> technologies;
-	
-	
-	
 
+	/**
+	 * technologies and project details are mapped by using the ManyToMany with a
+	 * Join table. Here Project is the owner of the table and technologies will
+	 * refer to the same table only
+	 */
+	@ManyToMany(fetch = FetchType.LAZY, cascade = {
+			CascadeType.MERGE,
+			CascadeType.PERSIST,
+			CascadeType.REFRESH,
+			CascadeType.REMOVE
+	})
+	@JoinTable(name = "projects_technologies", joinColumns = @JoinColumn(name = "project_id"),
+	inverseJoinColumns = @JoinColumn(name = "technology_id"))
+	@JsonProperty(value = "technologies_used")
+	private List<Technology> technologies = new ArrayList<>();
+	
+	/**
+	 * No argument Constructor. It is used for the JPA Transactions
+	 */
+	public ProjectDetail() {}
+
+	/**
+	 * @param projectName Name of the Project
+	 * @param teamSize    Size of team
+	 * @param startDate   Strat date of the project
+	 * @param endDate     End date of the Project
+	 * @param description Summary about the Project
+	 */
+	public ProjectDetail(String projectName, int teamSize, LocalDate startDate, LocalDate endDate, String description) {
+		this.projectName = projectName;
+		this.teamSize = teamSize;
+		this.startDate = startDate;
+		this.endDate = endDate;
+		this.description = description;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getProjectName() {
+		return projectName;
+	}
+
+	public void setProjectName(String projectName) {
+		this.projectName = projectName;
+	}
+
+	public int getTeamSize() {
+		return teamSize;
+	}
+
+	public void setTeamSize(int teamSize) {
+		this.teamSize = teamSize;
+	}
+
+	public LocalDate getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(LocalDate startDate) {
+		this.startDate = startDate;
+	}
+
+	public LocalDate getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(LocalDate endDate) {
+		this.endDate = endDate;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public List<Technology> getTechnologies() {
+		return technologies;
+	}
+
+	public void setTechnologies(List<Technology> technologies) {
+		this.technologies = technologies;
+	}
 }
--- src/main/java/org/fourstack/infomanagement/models/Technology.java
+++ src/main/java/org/fourstack/infomanagement/models/Technology.java
@@ -1,9 +1,37 @@
 package org.fourstack.infomanagement.models;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+/**
+ * Class <b><i>Technology</i></b> is an Entity class used to store the
+ * Technologies used by the Project.
+ * <p>
+ * In this class both 'technology_name' and 'version' combinedly acts as unique
+ * constraint
+ * </p>
+ * 
+ * @author Manjunath_HM
+ *
+ */
+@Entity
+@Table(name = "technologies", uniqueConstraints = {
+		@UniqueConstraint(name = "uniqueTechnologyConstraint", columnNames = { "technology_name", "version" }) })
 public class Technology implements Serializable {
 
 	/**
@@ -11,13 +39,25 @@ public class Technology implements Serializable {
 	 */
 	private static final long serialVersionUID = 5157408858732324079L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
+	@Column(name = "technology_name")
 	@JsonProperty(value = "technology_name")
 	private String name;
 
+	@Column(name = "version")
 	@JsonProperty(value = "technology_version")
 	private String version;
+	
+	/**
+	 * Here we mentioned that the joining will be taken care by the technologies
+	 * variable in ProjectDetail
+	 */
+	@ManyToMany(mappedBy = "technologies", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@JsonIgnore
+	private List<ProjectDetail> projects = new ArrayList<>();
 
 	/**
 	 * No argument Constructor
@@ -59,6 +99,53 @@ public class Technology implements Serializable {
 
 	public void setVersion(String version) {
 		this.version = version;
+	}
+
+	public List<ProjectDetail> getProjects() {
+		return projects;
+	}
+
+	public void setProjects(List<ProjectDetail> projects) {
+		this.projects = projects;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int hashValue = 1;
+		hashValue = prime * hashValue + this.getClass().getName().hashCode();
+		
+		//compute hashValue for name and version, then add them to initial hashValue
+		hashValue = prime * hashValue + ((name != null) ? name.hashCode() : 0);
+		hashValue = prime * hashValue + ((version != null) ? version.hashCode() : 0);
+		
+		return hashValue;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null || getClass() != obj.getClass())
+			return false;
+
+		Technology other = (Technology) obj;
+		/*
+		 * if name is null, check with other.getName() and other.getName() is not null
+		 * return false. if name is not null, then compare both values using equals()
+		 * method
+		 */
+		if ((this.getName() == null) ? other.getName() != null : this.getName().equals(other.getName()))
+			return false;
+
+		if (this.getVersion() == null ? other.getVersion() != null : this.getVersion().equals(other.getName()))
+			return false;
+
+		/*
+		 * if (this.getName() == null) { if (other.name != null) return false; } else if
+		 * (!this.getName().equals(other.name)) return false;
+		 */
+		return true;
 	}
 
 	@Override
