f4011973318863d5f99a5f9eba9d4bf401027da9
Hm <Manjunath_Hm@Dellteam.com>
2020-05-14 13:55:33 +0530

    Added page number and offset values into the payload

--- src/main/java/org/fourstack/infomanagement/payload/PersonRequestPayload.java
+++ src/main/java/org/fourstack/infomanagement/payload/PersonRequestPayload.java
@@ -1,5 +1,7 @@
 package org.fourstack.infomanagement.payload;
 
+import java.io.Serializable;
+
 import org.fourstack.infomanagement.codetype.GenderType;
 import org.fourstack.infomanagement.codetype.MaritalStatus;
 
@@ -13,38 +15,52 @@ import com.fasterxml.jackson.annotation.JsonProperty;
  * @author Manjunath_HM
  *
  */
-public class PersonRequestPayload {
+public class PersonRequestPayload implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7821625833208964311L;
+
 	@JsonProperty(value = "first_name")
 	private String firstName;
-	
+
 	@JsonProperty(value = "last_name")
 	private String lastName;
-	
+
 	@JsonProperty(value = "gender")
 	private GenderType gender;
-	
+
 	@JsonProperty(value = "marital_status")
 	private MaritalStatus maritalStatus;
-	
+
 	@JsonProperty(value = "prim_contact_num")
 	private String primaryContactNumber;
-	
+
 	@JsonProperty(value = "mail_id")
 	private String mailId;
-	
+
 	@JsonProperty(value = "state")
 	private String state;
-	
+
 	@JsonProperty(value = "country")
 	private String country;
-	
+
 	@JsonProperty(value = "pincode")
 	private String pincode;
-	
+
+	@JsonProperty(value = "page_num")
+	private int pageNumber;
+
+	@JsonProperty(value = "page_offset")
+	private int pageOffset;
+
 	public PersonRequestPayload() {
 	}
 
 	/**
+	 * Parameterized Constructor for PersonRequestPayload Object excluding page
+	 * number and page offset
+	 * 
 	 * @param firstName
 	 * @param lastName
 	 * @param gender
@@ -66,6 +82,38 @@ public class PersonRequestPayload {
 		this.state = state;
 		this.country = country;
 		this.pincode = pincode;
+	}
+
+	/**
+	 * Parameterized Constructor for PersonRequestPayload Object including page
+	 * number and page offset
+	 * 
+	 * @param firstName
+	 * @param lastName
+	 * @param gender
+	 * @param maritalStatus
+	 * @param primaryContactNumber
+	 * @param mailId
+	 * @param state
+	 * @param country
+	 * @param pincode
+	 * @param pageNumber
+	 * @param pageOffset
+	 */
+	public PersonRequestPayload(String firstName, String lastName, GenderType gender, MaritalStatus maritalStatus,
+			String primaryContactNumber, String mailId, String state, String country, String pincode, int pageNumber,
+			int pageOffset) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.gender = gender;
+		this.maritalStatus = maritalStatus;
+		this.primaryContactNumber = primaryContactNumber;
+		this.mailId = mailId;
+		this.state = state;
+		this.country = country;
+		this.pincode = pincode;
+		this.pageNumber = pageNumber;
+		this.pageOffset = pageOffset;
 	}
 
 	public String getFirstName() {
@@ -138,5 +186,21 @@ public class PersonRequestPayload {
 
 	public void setPincode(String pincode) {
 		this.pincode = pincode;
+	}
+
+	public int getPageNumber() {
+		return pageNumber;
+	}
+
+	public void setPageNumber(int pageNumber) {
+		this.pageNumber = pageNumber;
+	}
+
+	public int getPageOffset() {
+		return pageOffset;
+	}
+
+	public void setPageOffset(int pageOffset) {
+		this.pageOffset = pageOffset;
 	}
 }
