da9a24a793cf1cd6b7173c99850fe1e763bcb04b
Hm <Manjunath_Hm@Dellteam.com>
2020-05-14 12:07:43 +0530

    Updated mailId column to case insensitive using @ColumnTransformer

--- src/main/java/org/fourstack/infomanagement/models/ContactInfo.java
+++ src/main/java/org/fourstack/infomanagement/models/ContactInfo.java
@@ -10,6 +10,8 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 
+import org.hibernate.annotations.ColumnTransformer;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 /**
@@ -44,6 +46,7 @@ public class ContactInfo implements Serializable {
 	private String secondaryContactNum;
 
 	@Column(name = "mailId")
+	@ColumnTransformer(read = "LOWER(mailId)", write = "LOWER(?)")
 	@JsonProperty(value = "mail_id")
 	private String mailId;
 
