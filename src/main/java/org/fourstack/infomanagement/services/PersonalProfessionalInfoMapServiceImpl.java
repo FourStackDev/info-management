2823a9ef0adbc6731e1a2c471435f948a472f235
Hm <Manjunath_Hm@Dellteam.com>
2020-05-26 16:18:14 +0530

    Added code to check and map the Person and Profession Mappings

--- src/main/java/org/fourstack/infomanagement/services/PersonalProfessionalInfoMapServiceImpl.java
+++ src/main/java/org/fourstack/infomanagement/services/PersonalProfessionalInfoMapServiceImpl.java
@@ -1,5 +1,7 @@
 package org.fourstack.infomanagement.services;
 
+import java.util.Optional;
+
 import org.fourstack.infomanagement.models.Person;
 import org.fourstack.infomanagement.models.PersonalProfessionalInfoMap;
 import org.fourstack.infomanagement.models.ProfessionalInfo;
@@ -44,15 +46,54 @@ public class PersonalProfessionalInfoMapServiceImpl implements PersonalProfessio
 				? professionalService.saveProfessionalInfo(payload.getProfessionalInfo())
 				: null;
 
-		PersonalProfessionalInfoMap infoMap = new PersonalProfessionalInfoMap();
-		infoMap.setPersonId(person != null ? person.getId() : null);
-		infoMap.setProfessionalId(profession != null ? profession.getId() : null);
-
+		PersonalProfessionalInfoMap infoMap = mapPersonAndProfessionInfo(person, profession);
+		
 		mapRepository.save(infoMap);
 		
 		payload.setPerson(person);
 		payload.setProfessionalInfo(profession);
 		return payload;
+	}
+
+	@Override
+	public Optional<PersonalProfessionalInfoMap> getPayloadByPersonId(Long personId) {
+		return mapRepository.findByPersonId(personId);
+	}
+
+	@Override
+	public Optional<PersonalProfessionalInfoMap> getPayloadByProfessionalId(Long professionalId) {
+		return mapRepository.findByProfessionalId(professionalId);
+	}
+	
+	/**
+	 * Method to map the {@link Person} info and {@link ProfessionalInfo}. Fetch the
+	 * mapping if already exists and map the Objects properly, so that one Person
+	 * can have only one ProfessionalInfo mapping.
+	 * 
+	 * @param person     {@link Person} Object
+	 * @param profession {@link ProfessionalInfo} Object
+	 * @return {@link PersonalProfessionalInfoMap} Object after properly mapping.
+	 */
+	private PersonalProfessionalInfoMap mapPersonAndProfessionInfo(Person person, ProfessionalInfo profession) {
+		// Create new Object and Set the values
+		PersonalProfessionalInfoMap infoMap = new PersonalProfessionalInfoMap();
+		infoMap.setPersonId(person != null ? person.getId() : null);
+		infoMap.setProfessionalId(profession != null ? profession.getId() : null);
+
+		// Check if already a Entry exist for the Person or Profession in the Mapping
+		// table.
+		PersonalProfessionalInfoMap infoMap2 = null;
+		if (person != null)
+			infoMap2 = getPayloadByPersonId(person.getId()).orElse(null);
+		if (infoMap2 == null && profession != null)
+			infoMap2 = getPayloadByProfessionalId(profession.getId()).orElse(null);
+
+		// Fetch Mapping information and Set if already Exists
+		if (infoMap2 != null)
+			infoMap.setId(infoMap2.getId());
+
+		// return final Mapping value
+		return infoMap;
 	}
 
 }
