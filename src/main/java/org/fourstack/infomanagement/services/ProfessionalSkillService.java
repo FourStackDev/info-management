78a6cc73608ce5c2ba93c86d28fee2f300dc566c
Hm <Manjunath_Hm@Dellteam.com>
2020-06-18 13:48:25 +0530

    Updated Service class and Test cases for ProfessionalSkillService

--- src/main/java/org/fourstack/infomanagement/services/ProfessionalSkillService.java
+++ src/main/java/org/fourstack/infomanagement/services/ProfessionalSkillService.java
@@ -7,8 +7,10 @@ import org.fourstack.infomanagement.models.ProfessionalSkill;
 public interface ProfessionalSkillService {
 
 	public List<ProfessionalSkill> getProfessionalSkills();
-	
+
 	public ProfessionalSkill getProfessionalSkillById(Long id);
-	
+
 	public ProfessionalSkill saveProfessionalSkill(ProfessionalSkill professionalSkill);
+
+	public List<ProfessionalSkill> saveAllProfessionalSkills(List<ProfessionalSkill> professionalSkills);
 }
